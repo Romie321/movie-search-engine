197d39ad7cc380fb3b9afd5b696bd803cd45397b
Romie321 <raguilera321@gmail.com>
2026-08-10 23:50:27 -0700

    feat: render MovieCard components in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,16 @@ import "./App.css";
 import MovieCard from "./components/MovieCard";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <movieCard
+        movie={{ title: "Example Movie", releaseDate: "2023-01-01" }}
+      />
+      <MovieCard
+        movie={{ title: "Another Movie", releaseDate: "2023-02-01" }}
+      />
+    </>
+  );
 }
 
 export default App;
