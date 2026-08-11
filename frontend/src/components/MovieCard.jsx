be9a822945f92ed42166a4060d6f63ab490c3ec4
Romie321 <raguilera321@gmail.com>
2026-08-10 23:33:12 -0700

    feat: add favorite button to MovieCard component

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -3,6 +3,9 @@ function MovieCard({ movie }) {
     <div className="movie-card">
       <div className="movie-poster">
         <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+            <button className="favorite-btn" onClick={}></button>
+        </div>
       </div>
     </div>
   );
