2abd7c44b379b9cdc467524ab9974a3546e7c3f9
Romie321 <raguilera321@gmail.com>
2026-08-10 23:39:42 -0700

    feat: implement favorite button click handler in MovieCard component

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,15 +1,16 @@
 function MovieCard({ movie }) {
-  function handleFavoriteClick() {}
+  function handleFavoriteClick() {
+    alert("clicked");
+  }
 
   return (
     <div className="movie-card">
       <div className="movie-poster">
         <img src={movie.url} alt={movie.title} />
         <div className="movie-overlay">
-          <button
-            className="favorite-btn"
-            onClick={handleFavoriteClick}
-          ></button>
+          <button className="favorite-btn" onClick={handleFavoriteClick}>
+            &#9733;
+          </button>
         </div>
       </div>
     </div>
