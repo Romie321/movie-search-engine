85172c6b6c82f9c7ea4b8168288865921f53b72b
Romie321 <raguilera321@gmail.com>
2026-08-11 00:10:09 -0700

    refactor: remove MovieCard component rendering from App and add empty Favorites and Home pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";
 
 function App() {
-  return (
-    <>
-      <movieCard
-        movie={{ title: "Example Movie", releaseDate: "2023-01-01" }}
-      />
-      <MovieCard
-        movie={{ title: "Another Movie", releaseDate: "2023-02-01" }}
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
