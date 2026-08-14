9d9acb329c221f3c4894d60930eb97037cff21be
Romie321 <raguilera321@gmail.com>
2026-08-14 00:29:18 -0700

    feat: add NavBar component to App layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <main className="main-content">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
