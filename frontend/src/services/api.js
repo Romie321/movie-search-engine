748e25d6da077f0bd682d97fe319c76795b97645
Romie321 <raguilera321@gmail.com>
2026-08-16 00:11:29 -0700

    feat: implement getPopularMovies function to fetch popular movies from API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,2 +1,6 @@
 const API_KEY = "edded60ab8c03cfc342e282d61a210be";
 const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = aysnc () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+}
