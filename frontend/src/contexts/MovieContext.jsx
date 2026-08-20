6c23ad8f1bf881052d2b3d06f946146eb16ec1fc
Romie321 <raguilera321@gmail.com>
2026-08-20 00:06:32 -0700

    feat: add useEffect to MovieProvider for retrieving favorites from localStorage

--- frontend/src/contexts/MovieContext.jsx
+++ frontend/src/contexts/MovieContext.jsx
@@ -7,5 +7,9 @@ export const useMovieContext = () => useContext(MovieContext);
 export const MovieProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
 
+  useEffect(() => {
+    const storedFavs = localStorage.getItem("favorites");
+  }, []);
+
   return <MovieContext.Provider>{children}</MovieContext.Provider>;
 };
