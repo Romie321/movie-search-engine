4532f6a495a53180e4c2d439bd6f8d817f6874ad
Romie321 <raguilera321@gmail.com>
2026-08-22 00:04:00 -0700

    fix: correct isFavorites to isFavorite in MovieCard component

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -2,7 +2,7 @@ import "../css/MovieCard.css";
 import { useMovieContext } from "../contexts/MovieContext";
 
 function MovieCard({ movie }) {
-  const { addFavorite, removeFavorites, isFavorites } = useMovieContext();
+  const { addFavorite, removeFavorites, isFavorite } = useMovieContext();
   const favorite = isFavorite(movie.id);
 
   function handleFavoriteClick() {
