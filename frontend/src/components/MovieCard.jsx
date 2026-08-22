07b9afddd46d251d834ae3cad510b9e13b154ec3
Romie321 <raguilera321@gmail.com>
2026-08-22 00:14:16 -0700

    fix: correct typo in addToFavorites function call in MovieCard component

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ function MovieCard({ movie }) {
   function handleFavoriteClick(e) {
     e.preventDefault();
     if (favorite) removeFromFavorites(movie.id);
-    else addToFavoritew(movie);
+    else addToFavorites(movie);
   }
 
   return (
